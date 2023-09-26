f479d90c009d3c8cf92e8be24d865a10fcbdaaef
francis <clyderitchie@yahoo.com>
2023-09-25 22:51:53 -0700

    Got the card to generate the weather for the day

--- assets/script.js
+++ assets/script.js
@@ -17,41 +17,53 @@ document.querySelector('#city-search').addEventListener('submit', function (even
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
+            renderWeather(data);
+            todayweather(data);
             var lon = data.coord.lon;
             var lat = data.coord.lat;
-            console.log(lon,lat);
+            // console.log(lon, lat);
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                 console.log('hello')
-            })
+                .then(response => response.json())
+                .then(data => {
+                    // console.log('hello')
+                })
         })
-
 });
 
 
-
 // function to call certain elements from the object to create a card within the html
-// function renderWeather(city) {
-//     var cardHtml =
-//         `<div class="card">
-// <img src=${city.weather.icon} class=card-img-top alt=${title}>
-// <div class="card-body>
-// <h5 class=card-title>${city.coord}</h5>
-// <p class="card-text">${city.main.temp}</p>
-// <p class="card-text">${city.wind.speed}</p>
-// <p class="card-text">${city.main.humidity}</p>
-// </div>
-//  </div>`
+function renderWeather(data) {
+    for (var i = 0; i < 5; i++) {
+        var cardHtml =
+            `<div class="card">
+                <img src='https://openweathermap.org/img/w/${data.weather[0].icon}.png' class=card-img-top alt='hi'>
+                <div class="card-body">
+                    <h5 class=card-title>City: ${data.name}</h5>
+                    <p class="card-text">Temp: ${data.main.temp}</p>
+                    <p class="card-text">Wind: ${data.wind.speed}</p>
+                    <p class="card-text">Humidity: ${data.main.humidity}</p>
+                </div>
+             </div>`
+    }
 
-//     var = {location, temp, wind, hum, icon}
-//     document.querySelector('#5day').innerHTML = cardHtml;
-// }
-// renderWeather();
+    document.querySelector('#day-forcast').setHTML(cardHtml);
+}
 
-
-
+// TODO: Render todays forcast
+function todayweather(data) {
+    var todayHtml = `<div class="card">
+    <img src='https://openweathermap.org/img/w/${data.weather[0].icon}.png' class=card-img-top alt='hi'>
+    <div class="card-body">
+        <h5 class=card-title>City: ${data.name}</h5>
+        <p class="card-text">Temp: ${data.main.temp}</p>
+        <p class="card-text">Wind: ${data.wind.speed}</p>
+        <p class="card-text">Humidity: ${data.main.humidity}</p>
+    </div>
+ </div>`
+ document.querySelector('#today-forcast').setHTML(todayHtml);
+}
+// TODO: Store last five of the user's searches
 
 
 // Then endpoint you will fetch to for current weather is:
@@ -63,46 +75,46 @@ document.querySelector('#city-search').addEventListener('submit', function (even
 
 
 
-var exampleObject = {
-    "coord": {
-        "lon": -122.4443,
-        "lat": 47.2529
-    },
-    "weather": [
-        {
-            "id": 800,
-            "main": "Clear",
-            "description": "clear sky",
-            "icon": "01n"
-        }
-    ],
-    "base": "stations",
-    "main": {
-        "temp": 62.13,
-        "feels_like": 60.91,
-        "temp_min": 56.55,
-        "temp_max": 66.36,
-        "pressure": 1011,
-        "humidity": 61
-    },
-    "visibility": 10000,
-    "wind": {
-        "speed": 5.75,
-        "deg": 260
-    },
-    "clouds": {
-        "all": 0
-    },
-    "dt": 1695442151,
-    "sys": {
-        "type": 2,
-        "id": 39016,
-        "country": "US",
-        "sunrise": 1695390981,
-        "sunset": 1695434927
-    },
-    "timezone": -25200,
-    "id": 5812944,
-    "name": "Tacoma",
-    "cod": 200
-}
+// var exampleObject = {
+//     "coord": {
+//         "lon": -122.4443,
+//         "lat": 47.2529
+//     },
+//     "weather": [
+//         {
+//             "id": 800,
+//             "main": "Clear",
+//             "description": "clear sky",
+//             "icon": "01n"
+//         }
+//     ],
+//     "base": "stations",
+//     "main": {
+//         "temp": 62.13,
+//         "feels_like": 60.91,
+//         "temp_min": 56.55,
+//         "temp_max": 66.36,
+//         "pressure": 1011,
+//         "humidity": 61
+//     },
+//     "visibility": 10000,
+//     "wind": {
+//         "speed": 5.75,
+//         "deg": 260
+//     },
+//     "clouds": {
+//         "all": 0
+//     },
+//     "dt": 1695442151,
+//     "sys": {
+//         "type": 2,
+//         "id": 39016,
+//         "country": "US",
+//         "sunrise": 1695390981,
+//         "sunset": 1695434927
+//     },
+//     "timezone": -25200,
+//     "id": 5812944,
+//     "name": "Tacoma",
+//     "cod": 200
+// }
